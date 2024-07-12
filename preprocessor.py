87e44cc89a29fda935691feb579245ba448f0031
yashmithaa <syash.mitha94@gmail.com>
2024-07-12 20:34:34 +0530

    roberta model

--- preprocessor.py
+++ preprocessor.py
@@ -5,7 +5,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
 import string
+from rich.console import Console
+from rich.table import Table
 
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForSequenceClassification
+from scipy.special import softmax
+from tqdm import tqdm
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
@@ -23,6 +29,7 @@ class Loader:
         dataset_config = self.config['dataset']
         self.data = pd.read_csv(dataset_config['path'], delimiter=dataset_config['delimiter'])
         return self.data
+    
 class DataCleaner:
     def __init__(self, config):
         self.config = config
@@ -31,6 +38,7 @@ class DataCleaner:
         data.dropna(inplace=True)
         data.drop_duplicates(inplace=True)
         return data
+    
 class TextPreprocessor:
     def __init__(self, config):
         self.config = config['preprocessing']['text']
@@ -87,7 +95,47 @@ class DataSplitter:
         self.test_data, self.validation_data = train_test_split(remaining_data, test_size=test_size)
 
         return self.train_data, self.test_data, self.validation_data
+    
+class Model:
+    def __init__(self, config):
+        self.config = config
+        self.MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL)
+        self.model = TFAutoModelForSequenceClassification.from_pretrained(self.MODEL)
 
+    def polarity_scores_roberta(self, example):
+        encoded_text = self.tokenizer(example, return_tensors='tf')
+        output = self.model(encoded_text)
+        scores = output.logits[0].numpy()
+        scores = softmax(scores)
+        scores_dict = {
+            'roberta_neg': scores[0],
+            'roberta_neu': scores[1],
+            'roberta_pos': scores[2]
+        }
+        return scores_dict
+    
+    def roberta(self, data, num_samples=5):
+        samples = data.head(num_samples)
+        results = []
+        for index, row in samples.iterrows():
+            text = row['text']
+            scores = self.polarity_scores_roberta(text)
+            results.append((text, scores))
+        return results
+    
+    def print_results(self, results):
+        table = Table(title="Results")
+        table.add_column("Text", justify="left")
+        table.add_column("Negative", justify="right")
+        table.add_column("Neutral", justify="right")
+        table.add_column("Positive", justify="right")
+        for text, scores in results:
+            table.add_row(text, f"{scores['roberta_neg']:.4f}", f"{scores['roberta_neu']:.4f}", f"{scores['roberta_pos']:.4f}")
+        console = Console()
+        console.print(table) 
+  
+       
 
 def main():
     config_path = 'config.yaml'
@@ -104,18 +152,22 @@ def main():
     # Preprocess data
     preprocessor = TextPreprocessor(config)
     data = preprocessor.preprocess_dataset(data)
-
+    print(f"Preprocessed data looks like,\n{data.head(5)}")
     # Split data
     splitter = DataSplitter(config)
     train_set, validation_set, test_set = splitter.split_data(data)
 
-    # just for verification
-    print(data.head(5))
-    print(f"Total size of data: {len(train_set)+len(test_set)+len(validation_set)}")
-    print(f"Training set size: {len(train_set)}")
-    print(f"Test set size: {len(test_set)}")
-    print(f"Validation set size: {len(validation_set)}")
-    print(validation_set.head(5))
+    table = Table(title="Dataset")
+    table.add_column("Train set")
+    table.add_column("Validation set")
+    table.add_column("Test set")
+    table.add_row(str(len(train_set)),str(len(validation_set)),str(len(test_set)))
+    console = Console()
+    console.print(table)
+
+    model = Model(config)
+    results = model.roberta(test_set,num_samples=5)
+    model.print_results(results)
 
 if __name__ == "__main__":
     main()
